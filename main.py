29e53d3fde58a8a132e1f19b78e09a5357100135
DiegoMellisG <diego.mellis@usach.cl>
2020-06-24 22:28:43 -0400

    CHG: Uso de diccionarios e implementado cplex

--- main.py
+++ main.py
@@ -1,24 +1,50 @@
 import numpy as np
 from scipy.spatial import distance
+import matplotlib.pyplot as plt
+from docplex.mp.model import Model
+import docplex.mp.solution as sol
 
-def distanceMatrix(filename):
+data = np.loadtxt('data/wi29.tsp.txt')
+n = data.shape[0] # NUMERO DE CIUDADES
+cities = [i for i in range(n)]
+arcos =[(i,j) for i in cities for j in cities if i!=j]
 
-    data = np.loadtxt(filename)
-    matrix = np.zeros(shape=(len(data),len(data)))
-    for row in data:
-        for row2 in data:
-            a = row[1:3]
-            b = row2[1:3]
-            matrix[int(row[0])-1, int(row2[0])-1] = distance.euclidean(a, b)        
-    return matrix
+coord_x = data[:, 1] # Primera columna 
+coord_y = data[:, 2] # Segunda columna
 
+distancia={(i, j): np.hypot(coord_x[i] - coord_x[j], coord_y[i] - coord_y[j]) for i,j in arcos}
+x=coord_x
+y=coord_y
 
-def main():
-    filename = "/wi29.tsp.txt"
-    matrix = distanceMatrix("data/" + filename)
-    np.savetxt("distance/" + filename, matrix, delimiter=" ")
+# Creando el modelo en Cplex
+mdl=Model('TSP')
 
+#Declaramos las variables a utilizar
+#los nombre y forma de llamar las variables es propia de CPLEX.
 
+x=mdl.binary_var_dict(arcos,name='x')
+d=mdl.continuous_var_dict(cities,name='d')
 
-if __name__ == "__main__":
-    main()
+mdl.minimize(mdl.sum(distancia[i]*x[i] for i in arcos))
+
+for c in cities:
+    mdl.add_constraint(mdl.sum(x[(i,j)] for i,j in arcos if i==c)==1, 
+                       ctname='out_%d'%c)
+
+for c in cities:
+    mdl.add_constraint(mdl.sum(x[(i,j)] for i,j in arcos if j==c)==1, 
+                       ctname='in_%d'%c)
+
+for i,j in arcos:
+    if j!=0:
+        mdl.add_indicator(x[(i,j)],d[i]+1==d[j], 
+                          name='order_(%d,_%d)'%(i, j))
+
+mdl.parameters.timelimit=150
+mdl.parameters.mip.strategy.branch=1
+mdl.parameters.mip.tolerances.mipgap=0.15
+
+solucion = mdl.solve(log_output=True)
+
+mdl.get_solve_status()
+solucion.display()
